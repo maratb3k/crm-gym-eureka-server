3aa61b200f10c5139a58d701f241cf33e5a54e23
Assem <maratb3k@gmail.com>
2024-12-12 00:05:04 +0500

    first commit

---
+++ src/test/java/com/example/crm_eureka_server/CrmEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crm_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
